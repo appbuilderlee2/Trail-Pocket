3aca791a5825d2bec690f0950db673c64da57181
appbuilderlee2 <appbuilderlee2@gmail.com>
2026-09-05 08:34:42 +0930

    Release v3.1.3 map-first mobile activity mode (sw.js)

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const PREFIX =
   "trail-pocket-shell:" + new URL(self.registration.scope).pathname + ":";
-const VERSION = PREFIX + "v3.1.2";
+const VERSION = PREFIX + "v3.1.3";
 const ASSETS = [
   "./backup.mjs",
   "./wake-lock.mjs",
