7245834575312f64a75863fa9c4e777c4476985b
appbuilderlee2 <appbuilderlee2@gmail.com>
2026-09-05 07:06:54 +0930

    Release Trail Pocket v3.1.0 mobile activity UI

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const PREFIX =
   "trail-pocket-shell:" + new URL(self.registration.scope).pathname + ":";
-const VERSION = PREFIX + "v3.0.2";
+const VERSION = PREFIX + "v3.1.0";
 const ASSETS = [
   "./backup.mjs",
   "./wake-lock.mjs",
