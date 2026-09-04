531734ee05e196307aad22a701846ff6349edd92
appbuilderlee2 <appbuilderlee2@gmail.com>
2026-09-05 07:32:36 +0930

    Release Trail Pocket v3.1.2 larger mobile map

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const PREFIX =
   "trail-pocket-shell:" + new URL(self.registration.scope).pathname + ":";
-const VERSION = PREFIX + "v3.1.1";
+const VERSION = PREFIX + "v3.1.2";
 const ASSETS = [
   "./backup.mjs",
   "./wake-lock.mjs",
