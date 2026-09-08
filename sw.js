f9ea65f7272fabf0b8a63f81870d43409b42b882
appbuilderlee2 <appbuilderlee2@gmail.com>
2026-09-09 01:16:18 +0930

    Bump cache after preview CI fixes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const PREFIX =
   "trail-pocket-shell:" + new URL(self.registration.scope).pathname + ":";
-const VERSION = PREFIX + "v4.1.0-r8";
+const VERSION = PREFIX + "v4.1.0-r9";
 const ASSETS = [
   "./unified-ui.mjs",
   "./unified-ui-base.mjs",
