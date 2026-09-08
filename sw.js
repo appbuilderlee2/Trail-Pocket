94e07c0d7f90ad84b47a4b707a7ddaaba81d36da
appbuilderlee2 <appbuilderlee2@gmail.com>
2026-09-09 00:23:34 +0930

    Bump cache after fixing activity panel overlap

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const PREFIX =
   "trail-pocket-shell:" + new URL(self.registration.scope).pathname + ":";
-const VERSION = PREFIX + "v4.1.0-r2";
+const VERSION = PREFIX + "v4.1.0-r3";
 const ASSETS = [
   "./unified-ui.mjs",
   "./unified-ui.css",
